27f68f76de133ab15c1edc8deaf021dec1da0c2b
anthony-le-goldenowl <anthony.le.goldenowl@gmail.com>
2022-04-29 11:17:10 +0700

    Add header and init prj

--- Gemfile
+++ Gemfile
@@ -3,6 +3,14 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.1.1'
 
+gem 'bootstrap', '~> 5.1.3'
+
+gem 'jquery-rails'
+
+gem 'font-awesome-rails'
+
+gem 'devise'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.5'
 # Use postgresql as the database for Active Record
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,23 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+
+import Rails from '@rails/ujs'
+import Turbolinks from 'turbolinks'
+import * as ActiveStorage from '@rails/activestorage'
+import 'channels'
+import * as bootstrap from 'bootstrap'
+import "@fortawesome/fontawesome-free/js/all";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+const images = require.context('../images', true);
+const imagePath = (name) => images(name, true);
+
+
+window.addEventListener('turbolinks:load', () => {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})
