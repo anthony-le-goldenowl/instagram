ebeca1a4a034f332c6f567184ec7435ec0dec3a9
anthony-le-goldenowl <anthony.le.goldenowl@gmail.com>
2022-05-23 11:07:15 +0700

    refactoring code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,26 @@
 class UsersController < ApplicationController
+  before_action :get_user, only: %i[edit show]
+
   def show
-    @user = User.find(params[:id])
     @post = @user.posts.order(created_at: :desc)
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
-  def update
+  def update_avatar
     if user_params[:avatar]
       if current_user.avatar.attach(user_params[:avatar])
         redirect_to edit_user_path(current_user.id)
       else
         render partial: 'change_avatar_modal', layout: false
       end
-    elsif current_user.update(user_params)
+    else
+      render :edit
+    end
+  end
+
+  def update
+    if current_user.update(user_params)
       redirect_to current_user
     else
       render :edit
@@ -26,6 +31,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def get_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :name, :website, :bio, :email, :phone, :gender, :avatar)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'search' => 'search#index'
+  patch '/users/:id/avatar/edit' => 'users#update_avatar', as: :edit_avatar
   root to: 'home#index'
 
   devise_for :users, controllers: { sessions: 'users/sessions' }
